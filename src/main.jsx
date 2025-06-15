5d17cb9eac20398ede920a96697ee7613ff070e5
pr0thi <harshpruthi2002@gmail.com>
2025-06-16 01:08:33 +0530

    VercelSpeedInsightsAdded

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <SpeedInsights/>
   </StrictMode>,
 )
 
@@ -14,10 +16,10 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js')
       .then(registration => {
-        console.log('SW registered: ', registration);
+        //console.log('SW registered: ', registration);
       })
       .catch(registrationError => {
-        console.log('SW registration failed: ', registrationError);
+        //console.log('SW registration failed: ', registrationError);
       });
   });
 }
